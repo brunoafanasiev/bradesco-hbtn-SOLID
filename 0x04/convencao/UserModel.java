c5cf1575fd2ff8d04ca1a821403a13a1dfaaac51
Bruno Afanasiev <bruno.afanasiev@gmail.com>
2025-10-06 16:03:59 -0300

    2. Convenções de Nomenclatura

---
+++ 0x04/convencao/UserModel.java
@@ -0,0 +1,19 @@
+public class UsersModels {
+
+    private int idUser;
+    private String descricao;
+
+    public static final int IDADE_MINIMA = 18;
+
+    public List<User> fetchUsers() {
+        return List.of();
+    }
+
+    public List<User> getAllUsers() {
+        return List.of();
+    }
+
+    public List<User> getUsers() {
+        return List.of();
+    }
+}
